d98e2235c4466481515dde202a39a7976b2e2d9e
Carlos Martinez <cmartinez@breakfreesolutions.com>
2020-01-22 17:52:08 -0600

    deployed AWS IAM role for nodes

--- eks/terraform/main.tf
+++ eks/terraform/main.tf
@@ -45,3 +45,34 @@ resource "aws_route_table" "terraform-eks-rd-routetable" {
     gateway_id = aws_internet_gateway.eks-rapiddeploy-gw.id
   }
 }
+
+# The following is required manage or retrieve data from other AWS Services
+
+resource "aws_iam_role" "rapiddeploy-node" {
+  name = "terraform-eks-rapiddeploy-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.rapiddeploy-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.rapiddeploy-node.name
+}
